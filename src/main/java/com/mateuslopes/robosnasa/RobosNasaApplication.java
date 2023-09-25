0e911b16595eb61d0d2573b7dc8d6561a0c5834f
Mateus Lopes <mateusfellipe2302@gmail.com>
2023-09-25 14:00:28 -0300

    Estrutura inicial

---
+++ src/main/java/com/mateuslopes/robosnasa/RobosNasaApplication.java
@@ -0,0 +1,13 @@
+package com.mateuslopes.robosnasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobosNasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobosNasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateuslopes/robosnasa/RobosNasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuslopes.robosnasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobosNasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
