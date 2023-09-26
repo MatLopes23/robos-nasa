6437a9cbfc70b02bd67dd233e612e86cd2d80fab
Mateus Lopes <mateusfellipe2302@gmail.com>
2023-09-26 01:33:35 -0300

    Adiciona loggers informativos ao controller

--- src/main/java/com/mateuslopes/robosnasa/controller/MarController.java
+++ src/main/java/com/mateuslopes/robosnasa/controller/MarController.java
@@ -4,6 +4,8 @@ import com.mateuslopes.robosnasa.command.CommandProcessor;
 import com.mateuslopes.robosnasa.enums.OrientacaoEnum;
 import com.mateuslopes.robosnasa.exception.RoboBadRequestException;
 import com.mateuslopes.robosnasa.model.PosicaoRobo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/rest/mars")
 public class MarController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MarController.class);
 
     CommandProcessor commandProcessor;
 
@@ -21,14 +25,22 @@ public class MarController {
 
     @PostMapping("/{comandos}")
     public ResponseEntity<String> moveRobot(@PathVariable String comandos) {
+        logger.info("Recebendo solicitação para mover o robô. Comandos: {}", comandos);
+
         PosicaoRobo position = new PosicaoRobo(0, 0, OrientacaoEnum.NORTH);
 
         try{
             commandProcessor.execute(position, comandos);
+
+            logger.info("Movimento bem-sucedido. Posição: {}", position);
             return ResponseEntity.ok(position.toString());
         } catch (RoboBadRequestException e){
+            logger.warn("Movimento irregular detectado. Comandos: {}", comandos);
+
             return ResponseEntity.badRequest().body(e.getMessage());
-         }catch (Exception e) {
+        } catch (Exception e) {
+            logger.error("Ocorreu um erro ao processar os comandos.", e);
+
             return ResponseEntity.badRequest().body("Erro ao processar os comandos: " + e.getMessage());
         }
 
