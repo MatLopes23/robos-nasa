7619f4049ffe1fb067d919275c7c2cbef98e55b0
Mateus Lopes <mateusfellipe2302@gmail.com>
2023-09-25 22:44:57 -0300

    Corrigi retorno da api

--- src/main/java/com/mateuslopes/robosnasa/controller/MarController.java
+++ src/main/java/com/mateuslopes/robosnasa/controller/MarController.java
@@ -4,6 +4,7 @@ import com.mateuslopes.robosnasa.command.CommandProcessor;
 import com.mateuslopes.robosnasa.enums.OrientacaoEnum;
 import com.mateuslopes.robosnasa.model.PosicaoRobo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,22 +19,15 @@ public class MarController {
     }
 
     @PostMapping("/{comandos}")
-    public String moveRobot(@PathVariable String comandos) {
+    public ResponseEntity<String> moveRobot(@PathVariable String comandos) {
         PosicaoRobo position = new PosicaoRobo(0, 0, OrientacaoEnum.NORTH);
         commandProcessor.execute(position, comandos);
 
         if(position.getRealizouMovimentoIrregular())
-            return "falhou";
+            return ResponseEntity.badRequest().body("Erro ao processar os comandos.");
 
-        return position.toString();
+        return ResponseEntity.ok(position.toString());
 
     }
 
-    private boolean isValidPosition(PosicaoRobo position) {
-        int x = position.getEixoX();
-        int y = position.getEixoY();
-
-        // Verificar se a posição está dentro dos limites (0 a 4 para x e y)
-        return x >= 0 && x <= 4 && y >= 0 && y <= 4;
-    }
 }
